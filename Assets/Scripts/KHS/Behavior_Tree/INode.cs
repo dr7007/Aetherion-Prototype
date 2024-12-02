7e5701bf01f19c5b88f84049877fce34a00802bc
dr7007 <dr7007@naver.com>
2024-12-02 18:02:06 +0900

    BT를 이용한 간단한 동작 Update

---
+++ Assets/Scripts/KHS/Behavior_Tree/INode.cs
@@ -0,0 +1,12 @@
+public interface INode
+{
+    public enum ENodeState
+    {
+        ENS_Running,
+        ENS_Success,
+        ENS_Failure,
+    }
+
+    public ENodeState Evaluate();
+
+}
---
+++ Assets/Scripts/KHS/MonsterAI.cs
@@ -0,0 +1,173 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Animator))]
+public class MonsterAI : MonoBehaviour
+{
+    [Header("Range")]
+    [SerializeField] private float detectRange = 10f;
+    [SerializeField] private float meleeAttackRange = 2f;
+
+    [Header("Movement")]
+    [SerializeField] private float movSpeed = 4f;
+    
+    private Vector3 originPos = Vector3.zero;
+    private BehaviorTreeRunner runnerBT = null;
+    [SerializeField]Transform detectedPlayerTr = null;
+    private Animator anim = null;
+
+    private const string _ATTACK_ANIM_STATE_NAME = "Attack";
+    private const string _ATTACK_ANIM_TRIGGER_NAME = "attack";
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        runnerBT = new BehaviorTreeRunner(SettingBT());
+        originPos = transform.position;
+    }
+
+    private void Update()
+    {
+        runnerBT.Operate();
+    }
+
+    INode SettingBT()
+    {
+        return new SelectorNode
+            (
+                new List<INode>()
+                {
+                    new SequenceNode
+                    (
+                        new List<INode>()
+                        {
+                            new ActionNode(CheckMeleeAttacking),
+                            new ActionNode(CheckEnemyWithinMeleeAttackRange),
+                            new ActionNode(DoMeleeAttack),
+                        }
+                    ),
+                    new SequenceNode
+                    (
+                        new List<INode>()
+                        {
+                            new ActionNode(CheckDetectEnemy),
+                            new ActionNode(MoveToDetectEnemy),
+                        }
+                    ),
+                    new ActionNode(MoveToOriginPosition)
+                }
+            );
+    }
+
+    bool IsAnimationRunning(string _stateName)
+    {
+        if(anim != null)
+        {
+            if (anim.GetCurrentAnimatorStateInfo(0).IsName(_stateName))
+            {
+                var normalizedTime = anim.GetCurrentAnimatorStateInfo(0).normalizedTime;
+
+                return normalizedTime != 0 && normalizedTime < 1f;
+            }
+        }
+
+        return false;
+    }
+
+    #region Attack Node
+    INode.ENodeState CheckMeleeAttacking()
+    {
+        if(IsAnimationRunning(_ATTACK_ANIM_STATE_NAME))
+        {
+            return INode.ENodeState.ENS_Running;
+        }
+        return INode.ENodeState.ENS_Success;
+    }
+
+    INode.ENodeState CheckEnemyWithinMeleeAttackRange()
+    {
+        if(detectedPlayerTr != null)
+        {
+            if(Vector3.SqrMagnitude(detectedPlayerTr.position - transform.position) < (meleeAttackRange * meleeAttackRange))
+            {
+                return INode.ENodeState.ENS_Success;
+            }
+        }
+
+        return INode.ENodeState.ENS_Failure;
+    }
+
+    INode.ENodeState DoMeleeAttack()
+    {
+        if(detectedPlayerTr != null)
+        {
+            anim.SetTrigger(_ATTACK_ANIM_TRIGGER_NAME);
+            return INode.ENodeState.ENS_Success;
+        }
+
+        return INode.ENodeState.ENS_Failure;
+    }
+    #endregion
+
+    #region Detect & Move Node
+    INode.ENodeState CheckDetectEnemy()
+    {
+        var overlapColliders = Physics.OverlapSphere(transform.position, detectRange, LayerMask.GetMask("Player"));
+
+        if(overlapColliders != null && overlapColliders.Length > 0)
+        {
+            detectedPlayerTr = overlapColliders[0].transform;
+
+            return INode.ENodeState.ENS_Success;
+        }
+
+        detectedPlayerTr = null;
+
+        return INode.ENodeState.ENS_Failure;
+    }
+
+    INode.ENodeState MoveToDetectEnemy()
+    {
+        if(detectedPlayerTr != null)
+        {
+            if (Vector3.SqrMagnitude(detectedPlayerTr.position - transform.position) < (meleeAttackRange * meleeAttackRange))
+            {
+                anim.SetBool("isWalking", false);
+                return INode.ENodeState.ENS_Success;
+            }
+            anim.SetBool("isWalking", true);
+            transform.position = Vector3.MoveTowards(transform.position, detectedPlayerTr.position, Time.deltaTime * movSpeed);
+
+            return INode.ENodeState.ENS_Running;
+        }
+        anim.SetBool("isWalking", false);
+        return INode.ENodeState.ENS_Failure;
+    }
+    #endregion
+
+    #region Move Origin Pos Node
+    INode.ENodeState MoveToOriginPosition()
+    {
+        if(Vector3.SqrMagnitude(originPos - transform.position) < float.Epsilon * float.Epsilon)
+        {
+            anim.SetBool("isWalking", false);
+            return INode.ENodeState.ENS_Success;
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+            transform.position = Vector3.MoveTowards(transform.position, originPos, Time.deltaTime * movSpeed);
+            return INode.ENodeState.ENS_Running;
+        }
+    }
+    #endregion
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(this.transform.position, detectRange);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(this.transform.position, meleeAttackRange);
+    }
+}
