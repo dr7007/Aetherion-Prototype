f6dcf2b44b4e1c167f661f33f3fbb8dfb9570695
DESKTOP-VKT9SVB\user <ksu4250@gmail.com>
2024-12-03 12:48:51 +0900

    콤보어택 기능만 구현?, 플레이어 움직임 개선

--- Assets/Scripts/KSU/PlayerControl.cs
+++ Assets/Scripts/KSU/PlayerControl.cs
@@ -10,6 +10,7 @@ public class PlayerControl : MonoBehaviour
     public float moveSpeed = 3f;
 
 
+    private bool isAttacking = false;
     private Animator anim;
     private CharacterController controller;
     private void Start()
@@ -20,42 +21,23 @@ public class PlayerControl : MonoBehaviour
 
     public void Update()
     {
-        // 중력구현
-        Vector3 gravity = new Vector3(0f, -9.81f * Time.deltaTime, 0f);
-        controller.Move(gravity);
+        CheckAttack();
 
-        // 카메라 바라보는 방향으로 방향설정
-        Vector3 viewDir = transform.position - new Vector3(cameraTr.position.x, transform.position.y, cameraTr.position.z);
-        orientation.forward = viewDir.normalized;
-
-        // 움직임 감지
+        // 키입력 감지
         float axisH = Input.GetAxis("Horizontal");
         float axisV = Input.GetAxis("Vertical");
 
-        // 카메라 방향으로 움직임 감지했을때 값 저장
-        Vector3 inputDir = orientation.forward * axisV + orientation.right * axisH;
-
-        // 움직였을때 플레이어 방향 조절
-        if (inputDir != Vector3.zero)
+        if (!isAttacking)
         {
-            transform.forward = Vector3.Slerp(transform.forward, inputDir.normalized, Time.deltaTime * rotSpeed);
-            anim.SetBool("IsWalking", true);
-        }
-        else
-        {
-            anim.SetBool("IsWalking", false);
+            PlayerMove(axisV, axisH);
         }
 
+        // 스페이스바 눌렀을때
         if (Input.GetKeyDown(KeyCode.Space))
         {
             StartCoroutine(RollAnimCoroutine());
         }
 
-        // 해당 방향으로 이동
-        if (controller.isGrounded)
-        {
-            controller.SimpleMove(inputDir * moveSpeed);
-        }
     }
 
     private IEnumerator RollAnimCoroutine()
@@ -66,5 +48,65 @@ public class PlayerControl : MonoBehaviour
 
         anim.SetBool("IsSpace", false);
     }
-}
 
+    // 플레이어 이동 관련 함수
+    private void PlayerMove(float _axisV, float _axisH)
+    {
+        // 중력구현
+        Vector3 gravity = new Vector3(0f, -9.81f * Time.deltaTime, 0f);
+        controller.Move(gravity);
+
+        // 카메라 바라보는 방향으로 방향설정
+        Vector3 viewDir = transform.position - new Vector3(cameraTr.position.x, transform.position.y, cameraTr.position.z);
+        orientation.forward = viewDir.normalized;
+
+        // 카메라 방향으로 움직임 감지했을때 값 저장
+        Vector3 inputDir = orientation.forward * _axisV + orientation.right * _axisH;
+
+        // 움직였을때 플레이어 방향 조절
+        if (inputDir != Vector3.zero)
+        {
+            transform.forward = Vector3.Slerp(transform.forward, inputDir.normalized, Time.deltaTime * rotSpeed);
+            anim.SetBool("IsMoving", true);
+        }
+        else
+        {
+            anim.SetBool("IsMoving", false);
+        }
+
+        // 왼쪽 씨프트 눌렀을때
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            anim.SetBool("IsFast", true);
+            moveSpeed = 6f;
+        }
+        else
+        {
+            anim.SetBool("IsFast", false);
+            moveSpeed = 3f;
+        }
+
+        // 해당 방향으로 이동
+        if (controller.isGrounded)
+        {
+            controller.SimpleMove(inputDir * moveSpeed);
+        }
+    }
+
+
+    // 현재 공격중인지 확인하는 함수
+    private void CheckAttack()
+    {
+        AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+
+        // 현재 서브스테이트 내부 상태 확인
+        if (stateInfo.IsName("SwordAttack.combo1") || stateInfo.IsName("SwordAttack.combo2") || stateInfo.IsName("SwordAttack.combo3"))
+        {
+            isAttacking = true;
+        }
+        else
+        {
+            isAttacking = false;
+        }
+    }
+}
